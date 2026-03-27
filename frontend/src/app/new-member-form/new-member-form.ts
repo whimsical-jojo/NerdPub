69fba8240ebad9316889bb66fbd6122e86bcad41
MarcoBisco <marco19febbraio@gmail.com>
2026-03-27 16:32:37 +0100

    Creazione componente NewMemberForm

---
+++ frontend/src/app/new-member-form/new-member-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-member-form',
+  imports: [],
+  templateUrl: './new-member-form.html',
+  styleUrl: './new-member-form.css',
+})
+export class NewMemberForm {
+
+}
